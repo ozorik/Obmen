0445c96c7b3252a1d5c1abe964ecf755193cd0d2
ozorik <ozorik@ya.ru>
2024-11-28 09:08:36 +0300

    вариант от преподавателя - две базовые валюты у одной целевой

--- Обмен.py
+++ Обмен.py
@@ -6,53 +6,46 @@ from tkinter import ttk
 
 
 def update_b_label(event):
-    code = b_combobox.get()
+    code = base_combobox.get()
     name = cur[code]
     b_label.config(text=name)
 
 
 def update_t_label(event):
-    code = t_combobox.get()
+    code = target_combobox.get()
     name = cur[code]
     t_label.config(text=name)
 
 
 def exchange():
-    t_code = t_combobox.get()
-    b_codes = [b_combobox.get(), b2_combobox.get()]
+    target_code = target_combobox.get().lower()
+    base_code = base_combobox.get().lower()
 
-    if t_code and all(b_codes):
+
+    if target_code and base_code:
         try:
-            result_text = ""
-            for b_code in b_codes:
-                response = requests.get(f'https://open.er-api.com/v6/latest/{b_code}')
-                response.raise_for_status()
-                data = response.json()
-                if t_code in data['rates']:
-                    exchange_rate = data['rates'][t_code]
-                    t_name = cur[t_code]
-                    b_name = cur[b_code]
-                    result_text += f"Курс: 1 {b_name} = {exchange_rate:.2f} {t_name}\n"
-                else:
-                    mb.showerror("Ошибка!", f"Валюта {t_code} не найдена!")
-                    return
-                mb.showinfo("Курсы обмена", result_text.strip())
+            #url = f'https://www.coingecko.com/api/btc'
+            response = requests.get(f'https://api.coingecko.com/api/{base_code}')
+            response.raise_for_status()
+            data = response.json()
+
+            if target_code in data['rates']:
+                exchange_rate = data['rates'][target_code]
+                base = cur[base_code]
+                target = cur[target_code]
+                mb.showinfo("Курс обмена", f"Курс {exchange_rate:.1f} {target} за 1 {base}")
+            else:
+                mb.showerror("Ошибка", f"Валюта {target_code} не найдена")
         except Exception as e:
-            (mb.showerror("Ошибка", f"Произошла ошибка: {e}."))
+            mb.showerror("Ошибка", f"Ошибка: {e}")
     else:
-        mb.showwarning("Внимание!", "Введите коды валют!")
+        mb.showwarning("Внимание", "Выберите коды валют")
+
 
 cur = {
-'RUB': 'Российский рубль',
-'EUR': 'Евро',
-'GBP': 'Британский фунт стерлингов',
-'JPY': 'Японская йена',
-'CNY': 'Китайский юань',
-'KZT': 'Казахский тенге',
-'UZS': 'Узбекский сум',
-'AED': 'Дирхам ОАЭ',
-'CAD': 'Канадский доллар',
-'USD': 'Американский доллар'
+'etc': 'Российский рубль',
+'btc': 'Евро',
+'usd': 'Американский доллар'
 }
 
 
@@ -61,23 +54,18 @@ window.title("Курсы обмена валют")
 window.geometry("360x420")
 
 Label(text="Базовая валюта").pack(padx=10, pady=10)
-b_combobox = ttk.Combobox(values=list(cur.keys()))
-b_combobox.pack(padx=10, pady=10)
-b_combobox.bind("<<ComboboxSelected>>", update_b_label)
+base_combobox = ttk.Combobox(values=list(cur.keys()))
+base_combobox.pack(padx=10, pady=10)
+base_combobox.bind("<<ComboboxSelected>>", update_b_label)
 b_label = ttk.Label()
 b_label.pack(padx=10, pady=10)
 
-Label(text="Вторая базовая валюта").pack(padx=10, pady=10)
-b2_combobox = ttk.Combobox(values=list(cur.keys()))
-b2_combobox.pack(padx=10, pady=10)
-b2_combobox.bind("<<ComboboxSelected>>", lambda event: b2_label.config(text=cur[b2_combobox.get()]))
-b2_label = ttk.Label()
-b2_label.pack(padx=10, pady=10)
+
 
 Label(text="Целевая валюта").pack(padx=10, pady=10)
-t_combobox = ttk.Combobox(values=list(cur.keys()))
-t_combobox.pack(padx=10, pady=10)
-t_combobox.bind("<<ComboboxSelected>>", update_t_label)
+target_combobox = ttk.Combobox(values=list(cur.keys()))
+target_combobox.pack(padx=10, pady=10)
+target_combobox.bind("<<ComboboxSelected>>", update_t_label)
 t_label = ttk.Label()
 t_label.pack(padx=10, pady=10)
 
@@ -86,13 +74,9 @@ Button(text="Получить курс обмена", command=exchange).pack(pad
 window.mainloop()
 
 
-'''import pprint
 
-result = requests.get("https://open.er-api.com/v6/latest/USD")
-data = json.loads(result.text)
-p = pprint.PrettyPrinter(indent=4)
 
-p.pprint(data)'''
+
 
 
 
